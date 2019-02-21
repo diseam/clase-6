b1bc58b5c78f142d7dd0ffb663011ca512211f40
diseam <47757313+diseam@users.noreply.github.com>
2019-02-21 13:50:10 -0300

    2

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 
 	// Partamos por una modificación al CSS = https://api.jquery.com/multiple-selector/
 
-	$("p").css("font-style", "italic");
+	$("p.estrofa").css("font-style", "italic");
 
 	// Agreguemos un elemento HTML antes de cada párrafo = https://api.jquery.com/before/
 
-	$("p").before("<h1 class=\"display-4\">Los dinosaurios</h1>");
+	$("p.estrofa").before("<h1 class=\"display-4\">Now, This Is Fun</h1>");
 
 	// Agreguemos una clase al elemento agregado = https://api.jquery.com/addClass/#addClass-className
 
@@ -16,39 +16,31 @@ $(document).ready(function(){
 
 	// Revisemos efectos básicos, condicionados por el click = https://api.jquery.com/category/effects/basics/
 
-	$("button#si").click(function() { 
-		$(".dinosaurio").show() 
-	});
-	 
-	$("button#no").click(function() { 
-		$(".dinosaurio").hide() 
-	});
-
-	$("button#sino").click(function() { 
-		$(".dinosaurio").toggle() 
+	$("button#tog").click(function() { 
+		$(".estrofa").toggle() 
 	});
 
 	// Cambiemos la foto de Charly García, según el input que esté chequeado
 
-	$("input:radio[name=charlies]").change(function() {
+	$("input:radio[name=depeches]").change(function() {
     
-        if (this.value == "joven") { 
-        	$('img#foto').attr('src','img/charly-joven.jpg');
-        	console.log("Escogiste al Charly "+this.value)
+        if (this.value == "bebes") { 
+        	$('img#foto').attr('src','img/depeche-mode-bebes.jpg');
+        	console.log("Escogiste a Depeche "+this.value)
         }
-        else if (this.value == "viejo") { 
-        	$('img#foto').attr('src','img/charly-viejo.jpg');
-        	console.log("Escogiste al Charly "+this.value)
+        else if (this.value == "joven") { 
+        	$('img#foto').attr('src','img/depeche-mode-joven.jpg');
+        	console.log("Escogiste a Depeche "+this.value)
         }
-        else if (this.value == "inflado") { 
-        	$('img#foto').attr('src','img/charly-inflado.jpg');
-        	console.log("Escogiste al Charly "+this.value)
+        else if (this.value == "adulto") { 
+        	$('img#foto').attr('src','img/depeche-mode-adulto.jpg');
+        	console.log("Escogiste a Depeche "+this.value)
         }
     });
 
     // Para cerrar, vamos a buscar información a un JSON (JavaScript Object Notation) en línea
 
-	$.getJSON('https://raw.githubusercontent.com/profesorfaco/clase-6/gh-pages/data.json', function (data) {
+	$.getJSON('https://raw.githubusercontent.com/diseam/clase-6/gh-pages/data.json', function (data) {
 		
 		// Un aviso en la consola de JavaScript para cuando se obtenga el JSON
 		
@@ -69,7 +61,9 @@ $(document).ready(function(){
 		// Finalmente usamos un ciclo para ir por cada disco, y agregarlo como ítem al listado de identidad discografía 			
 		
 			for (var a = 0; a < data.length; a++){
-				$( "ul#discografia" ).append( "<li><a href=\"" + data[a]["spotify"] + "\">" + data[a]["album"] +" ("+ data[a]["lanzamiento"] +")</a></li>" );
+				if (data[a]["lanzamiento"] < 2000){
+					$( "div#discografia" ).append("<div class=\"col-6 col-sm-4 mb-4\"><div class=\"card\"><img class=\"card-img-top\" src=\""+ data[a]["cover"] +"\"><div class=\"card-body\"><h5 class=\"card-title\">" + data[a]["album"] + "</h5><p class=\"card-text\"><a href=\"" + data[a]["spotify"] + "\">Escucha el disco en Spotify</a></p></div><div class=\"card-footer\"><a href=\" " + data[a]["youtube"] + "\" class=\"btn btn-sm btn-danger\">Single en Youtube</a></div></div></div>");
+				}
 			}			
 	
 	});
